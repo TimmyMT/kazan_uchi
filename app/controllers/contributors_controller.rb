da29803e34e6335e849ffa797d3a456bc54f29e1
Timmy_MT <mathcore_detected@mail.ru>
2020-01-10 14:25:07 +0500

    changed respond for download pdf links

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -3,7 +3,12 @@ class ContributorsController < ApplicationController
     @contributor = Contributor.find(params[:id])
     respond_to do |format|
       format.pdf do
-        render pdf: "#{@contributor.login}.pdf", template: "contributors/show.html.slim"
+        # render pdf: "#{@contributor.login}.pdf", template: "contributors/show.html.slim"
+        @pdf = render_to_string pdf: "#{@contributor.login}.pdf",
+                                template: "contributors/show.html.slim",
+                                encoding: "UTF-8"
+
+        send_data(@pdf, filename: "#{@contributor.login}.pdf", type: "application/pdf")
       end
     end
   end
